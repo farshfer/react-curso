c86d151d698f412808e0943e1e76aebc8a2a8e11
farshfer <estrada.farfer@gmail.com>
2022-07-23 09:27:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-9k9pvh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-9k9pvh)
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+const App = () => {
+  return <h1> hola mundo </h1>;
+};
+
+render(<App />, document.getElementById('root'));
